7e2ab2feb54a30cd4975e5289cc8f73edb8be152
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2023-06-03 16:33:45 +0530

    initated

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '528c053f3emsh1aec884a4167a34p1dde12jsn49b4ac3b060e',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
